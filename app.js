66f57c42387a236102e8c951592385f98f202bca
Gaurav Tiwari <gauravkrtiwari79@gmail.com>
2024-05-12 19:11:35 +0530

    update media queries

---
+++ app.js
@@ -0,0 +1,11 @@
+// function enableAside(){
+//     // Define the query
+//     const mediaQuery = window.matchMedia('(min-width: 600px)')
+//     if(mediaQuery.matches){
+//         let sidebar = document.getElementById("hamburgerMenu");
+//         sidebar.style = "
+//         ";
+//     }
+// }
+
+// document.getElementById("hamburgerMenu").addEventListener("click", displayDate); 
